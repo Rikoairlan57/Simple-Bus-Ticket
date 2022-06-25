c6e69ee5bf808b883e0ab1c605c80dff8ef7d945
Rikoairlan <rikoairlan57@gmail.com>
2022-06-25 16:14:04 +0700

    modif screen getstarted

--- lib/screens/getstarted.dart
+++ lib/screens/getstarted.dart
@@ -1,5 +1,8 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 import 'package:bus_ticket/screens/login_screen.dart';
+import 'package:lottie/lottie.dart';
 
 class GetStarted extends StatelessWidget {
   const GetStarted({ Key? key }) : super(key: key);
@@ -8,23 +11,60 @@ class GetStarted extends StatelessWidget {
   Widget build(BuildContext context) {
     
     return Scaffold(
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(20),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-             ElevatedButton(onPressed: (){
-              Navigator.of(context)
-              .pushReplacement(
-                MaterialPageRoute(
-                  builder: (context) => LoginScreen()
-                ));
-             }, child: Text("Getstarted"))
+        body: SafeArea(
+          child: Stack(
+            children: <Widget>[
+              Container(
+                child: FractionallySizedBox(
+                  heightFactor: 1.0,
+                  widthFactor: 1.0,
+                  child: DecoratedBox(
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: NetworkImage("https://i.ibb.co/74zHyKc/pexels-vh-s-2377915.jpg"),
+                        fit: BoxFit.fill,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Text(
+                        "Beli Ticket Bus Murah Ga Pake Ribet Di Bus Ticket aja",
+                        textAlign: TextAlign.center, 
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold
+                        )
+                      ),
+                      SizedBox(
+                        height: 15,
+                      ),
+                      ElevatedButton(
+                        onPressed: (){
+                          Navigator.of(context).pushReplacement(
+                            MaterialPageRoute(builder: (context) => LoginScreen())
+                          );
+                        }, 
+                        child: Text("GETSTARTED")
+                      ),
+                      SizedBox(
+                        height: 20,
+                      )
+                    ],
+                  ),
+                )
+              )
             ],
           ),
         ),
-      ),
+      
     );
   }
 }
