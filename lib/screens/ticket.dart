3b9885ee5c117eb3c753afd0fa8d5fb2abdaf6a1
Rikoairlan <rikoairlan57@gmail.com>
2022-06-26 20:02:33 +0700

    small modif page qrcode

--- lib/screens/ticket.dart
+++ lib/screens/ticket.dart
@@ -1,6 +1,10 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:bus_ticket/screens/home_screen.dart';
+
 import 'package:flutter/material.dart';
 import 'package:qr_flutter/qr_flutter.dart';
+
 
 class Ticket extends StatefulWidget {
   int total;
@@ -16,28 +20,45 @@ class _TicketState extends State<Ticket> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      
       appBar: AppBar(
         title: Text("Ticket"),
         centerTitle: true,
         automaticallyImplyLeading: false,
       ),
       body: Center(
-        child: Column(
-          children: [
-            QrImage(
-              data: total.toString(),
-              version: QrVersions.auto,
-              size: 200.0,
-            ),
-            ElevatedButton(
-                onPressed: () {
-                  Navigator.of(context).push(
-                      MaterialPageRoute(builder: (context) => HomeScreen()));
-                },
-                child: Text("OKe"))
-          ],
+        child: Container(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("Scane me to pay", style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
+              QrImage(
+                data: total.toString(),
+                version: QrVersions.auto,
+                size: 200.0,
+              ),
+              Container(
+                width: 200,
+                child: Center(
+                  child: Text(
+                    "Screenshot this Qrcode and next scane to your payment", 
+                    maxLines: 2,
+                    textAlign: TextAlign.center,
+                    )
+                  ),
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.of(context).push(
+                        MaterialPageRoute(builder: (context) => HomeScreen()));
+                  },
+                  child: Text("OKe"))
+            ],
+          ),
         ),
+        
       ),
+    
     );
   }
 }
